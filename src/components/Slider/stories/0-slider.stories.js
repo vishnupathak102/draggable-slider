1b804bde6645b92ab01923e071b9ac053a067613
Vishnu Pathak <vishnupathak1997@gmail.com>
2020-04-27 23:13:41 +0530

    Some Stories Changes

--- src/components/Slider/stories/0-slider.stories.js
+++ src/components/Slider/stories/0-slider.stories.js
@@ -14,13 +14,7 @@ export const RedColorSlider = () => <Slider value={25} color="red" />;
 
 export const NegativeValuedSlider = () => <Slider min={-100} max={-10} color="green" />;
 
-export const SliderWithStep5 = () => <Slider min={10} step={5} max={50} color="green" />;
+export const SliderWithSteps5 = () => <Slider min={100} step={5} max={150} color="green" />;
 
-
-storiesOf('Slider', module)
-  .add('Default Slider', DefaultSlider)
-  .add('Red Color Slider', RedColorSlider)
-  .add('Negative Value Slider', NegativeValuedSlider)
-  .add('Step 5 Slider', SliderWithStep5);
 
 
