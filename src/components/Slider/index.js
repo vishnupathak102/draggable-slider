d028eb8a9864cc2d0c116edf1f29e2c62acf015d
Vishnu Pathak <vishnupathak1997@gmail.com>
2020-04-27 13:25:03 +0530

    Component created

---
+++ src/components/Slider/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class Slider extends Component {
+    state = {
+        value: this.props.value || this.props.min
+    }
+
+    getValue = (event)=>{
+        const {value} = event.target;
+        const {callBack} = this.props;
+        this.setState((state, props)=>{ return {value: Number(value)}},()=>callBack && callBack(Number(value)));
+    }
+
+    render() {
+        const {value} = this.state
+        const {min, max, step, color} = this.props;
+        const sliderRelativeValue = ((value-min)*(100/(max-min))); 
+        const backgroundColor = color ;
+        return (
+            <div className="slider">
+            <div className="slider-box">
+                <span className="slider-box-indicator">{min}</span>
+                <div className="slider-box-container">
+                    <div className="slider-bar">
+                        <div className="slider-bar-fill" style={{background:backgroundColor, width: `${sliderRelativeValue}%`}}>
+                        </div>
+                    </div>
+                    <div className="slider-bar-value" style={{ width: `${sliderRelativeValue}%`}}>{value}</div>
+                  <input id="slider-input" type="range" min={min} max={max} step={step} value={value} onChange={this.getValue} className="slider-input" id="myRange"/>
+                </div>
+                <span className="slider-box-indicator">{max}</span>
+            </div>
+            </div> 
+        )
+    }
+}
+
+Slider.defaultProps={
+    min:0,
+    max:100,
+    step:1,
+    value:0,
+    color: "red"
+}
+
+Slider.propTypes={
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    value: PropTypes.number,
+    color : PropTypes.string
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from './components/Slider';
+ReactDOM.render(<Slider />, document.getElementById("root"));
